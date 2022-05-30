029f4d56223ee85023c768a8aa1019ad3d0b5683
Waddles1005 <dtoreo7@gmail.com>
2022-05-29 20:46:58 -0700

    controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
-import  Express  from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const express = require("express");
+import postRoutes from './routes/posts.js';
+//import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+// Connect to MongoDB
+const CONNECTION_URL ='mongodb+srv://Waddles:Waddles123@cluster0.xwsks3h.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
